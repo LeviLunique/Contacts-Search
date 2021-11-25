1dedc9bcc3f4a19671cb39b5a792c51272fb42c4
Levi Lunique <levi.lunique@gmail.com>
2021-11-25 17:57:31 -0500

    Initial Commit

---
+++ Contacts Search/Contacts_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_SearchApp.swift
+//  Contacts Search
+//
+//  Created by user204006 on 11/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
