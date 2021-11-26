f833e01971a438cbe7abdb2ced76bf13b9b27686
Levi Lunique <levi.lunique@gmail.com>
2021-11-25 21:20:19 -0500

    Search Bars and JSON API

--- Contacts Search/ContentView.swift
+++ Contacts Search/ContentView.swift
@@ -14,12 +14,37 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                SearchBar()
-                EmptyStateView()
+                SearchBar(searchTerm: $viewModel.searchTerm)
+                if viewModel.users.isEmpty {
+                    EmptyStateView()
+                } else {
+                    List(viewModel.users) { user in
+                        UserView(user: user)
+                    }
+                    .listStyle(PlainListStyle())
+                }
             }
             .navigationBarTitle("Contact Search")
         }
     }
+}
+
+struct UserView: View {
+    
+    @ObservedObject var user: UserViewModel
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading) {
+                Text(user.name)
+                Text(user.phone)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
+    }
+    
 }
 
 struct EmptyStateView: View {
@@ -40,7 +65,9 @@ struct EmptyStateView: View {
     
 struct SearchBar: UIViewRepresentable {
     typealias UIViewType = UISearchBar
-        
+    
+    @Binding var searchTerm: String
+    
     func makeUIView(context: Context) -> UISearchBar {
         let searchBar = UISearchBar(frame: .zero)
         searchBar.delegate = context.coordinator
@@ -53,10 +80,20 @@ struct SearchBar: UIViewRepresentable {
     }
         
     func makeCoordinator() -> SearchBarCoordinator {
-        return SearchBarCoordinator()
+        return SearchBarCoordinator(searchTerm: $searchTerm)
     }
         
     class SearchBarCoordinator: NSObject, UISearchBarDelegate {
+        @Binding var searchTerm: String
+        
+        init(searchTerm: Binding<String>) {
+            self._searchTerm = searchTerm
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchTerm = searchBar.text ?? ""
+            UIApplication.shared.windows.first { $0.isKeyWindow }?.endEditing(true)
+        }
     }
 }
     
--- Contacts Search/DataModel.swift
+++ Contacts Search/DataModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class DataModel {
     
     func getUsers(completion: @escaping ([User]) -> ()) {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             let users = try! JSONDecoder().decode([User].self, from: data!)
